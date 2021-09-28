c4597d8aeb1cbe596aef60580b806085bef09b9b
michaelporreca <michaelporreca@gmail.com>
2021-09-28 14:47:29 -0700

    added client side image upload

--- frontend/src/components/NewPost.js
+++ frontend/src/components/NewPost.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from "react-dom";
 import { useState } from 'react';
 import axios from 'axios'
 import actions from '../api';
@@ -13,14 +15,34 @@ function NewPost(props) {
         let res = await actions.createNewPost({ title, post })
         props.history.push('/all-posts')
     }
+
+    // Image Upload
+    const uploadedImage = React.useRef(null);
+
+    const handleImageUpload = e => {
+      const [file] = e.target.files;
+      if (file) {
+        const reader = new FileReader();
+        const {current} = uploadedImage;
+        current.file = file;
+        reader.onload = (e) => {
+            current.src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+      }
+    };
     
 
     return (
         <div>
             <label>New Post</label>
             <form onSubmit={handleSubmit}>
-                <input onChange={e => setTitle(e.target.value)} type="text" placeholder="Title" />
-                <input onChange={e => setPost(e.target.value)} type="text" placeholder="Post" />
+                <input onChange={e => setTitle(e.target.value)} type="text" placeholder="Title" /><br></br>
+                <input onChange={e => setPost(e.target.value)} type="text" placeholder="Post" /><br></br>
+                <input type="file" accept="image/*" multiple = "false" onChange={handleImageUpload} />
+                <div style={{height: "150px", width: "150px", border: "2px dashed black"}}>
+                    <img ref={uploadedImage} style={{width: "150px", height: "150px", position: "absolute" }} />
+                </div>
                 <button>Submit</button>
             </form>
         </div>

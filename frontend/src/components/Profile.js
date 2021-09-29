91cd968987067744f8551ec8024646b5f96e239f
michaelporreca <michaelporreca@gmail.com>
2021-09-29 09:23:59 -0700

    upaded files

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,22 +2,12 @@ import { useContext, useState, useEffect } from 'react';
 import TheContext from '../TheContext';
 import axios from 'axios'
 import actions from '../api'
-
 function Profile(props) {
-
-
-
-
-
     const [posts, setPosts] = useState([])
-    
-
     useEffect(async () => {
         let res = await actions.getAllPosts()
         setPosts(res.data.reverse())
     }, [])
-
-
     const ShowPosts = () => {
         return posts.map(eachPost => {
             return (
@@ -36,25 +26,17 @@ function Profile(props) {
             )
         })
     }
-
-
     let { user } = useContext(TheContext)
     return (
         <div>
             Profile My name is
             <div className="profileName"> Prop Driilling: {props.user?.name}</div>
-
-
             <div className="profileName"> Context: {user?.name}</div>
-
             <div className="MyPosts">
                 <h4>Here is my Post</h4>
-
                 <ShowPosts />
-
             </div>
         </div>
     );
 }
-
 export default Profile;

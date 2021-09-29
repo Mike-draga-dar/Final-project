d8ee7b45a5ab621c5debf91ea4df0171f35c4792
Dragan Cicic <your3mail>
2021-09-29 17:54:01 -0400

    done for today

--- frontend/src/components/Images.js
+++ frontend/src/components/Images.js
@@ -17,7 +17,17 @@ function Images() {
 
     return (
         <div>
+
             <img src={images.drinks?.[0]?.strDrinkThumb} alt="random Drinks" />
+
+
+            <div className="drinks">
+                <h2>{images.drinks?.[0]?.strDrink}</h2>
+                <h5> {images.drinks?.[0]?.strInstructions}</h5>
+
+
+            </div>
+
         </div>
     )
 }

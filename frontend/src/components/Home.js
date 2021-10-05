1d6866fd3324f3a74dd537acb7e0bdb75bfd68e5
Dragan Cicic <your3mail>
2021-10-05 13:59:10 -0400

    tuesday

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,54 +1,9 @@
-import { useEffect, useState } from "react"
-import axios from 'axios';
-import actions from '../api';
-import { Link } from 'react-router-dom'
-import CocktailDetails from "./CocktailDetails"
+import React from 'react';
 
-
-
-function Home() {
-
-    const [drinks, setDrinks] = useState([]);
-    let [limit, setLimit] = useState(10);
-    let [skip, setSkip] = useState(0)
-
-
-
-    useEffect(async () => {
-
-        let res = await actions.getAllDrinks({ limit, skip })
-        console.log(res.data)
-
-        setDrinks(res.data)
-    }, [skip])
-
-
-
-    const ShowDrinks = () => {
-        return (
-            <div className="all-drinks">
-                {drinks.map((drink) => {
-                    return <Link key={drink._id} to={`/drinks/${drink._id}`}>
-                        <img src={drink.image} alt="image" className="all-drinks-img" /><br></br>
-                        {drink.name}<br></br>
-                        <br></br>
-                    </Link>
-                })
-                }
-            </div>
-        )
-    }
-
-
+function Home(props) {
     return (
-        <div className="container">
-            <div className="red-header">
-                <p>All Drinks</p>
-            </div>
-            <br></br><br></br>
-            <ShowDrinks />
-            <button onClick={() => setSkip(Math.max(skip - 10), 0)}>Previous</button>
-            <button onClick={() => setSkip(skip + 10)}>Next</button>
+        <div>
+            Home
         </div>
     );
 }
---
+++ frontend/src/components/OldAllDrinks.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import actions from '../api'
+
+function AllDrinks(props) {
+    const [drinks, setDrinks] = useState([])
+
+    let [limit, setLimit] = useState(10);
+    let [skip, setSkip] = useState(0)
+
+    useEffect(async () => {
+        let res = await actions.getAllDrinks({ limit, skip })
+        setDrinks(res.data.reverse())
+    }, [])
+
+
+    const handleClick = async (whichPostId, i) => {
+        console.log('click', whichPostId)
+        let res = await actions.likePost(whichPostId)
+        console.log(res.data)
+        let newDrinks = [...drinks]
+        newDrinks[i] = res.data
+        setDrinks(newDrinks)
+    }
+    console.log(drinks)
+    const ShowPosts = () => {
+        return drinks.map((eachDrink, i) => {
+            return (
+                <div key={eachDrink._id}>
+                    <img src={eachDrink.userId?.imageUrl}></img>
+                    <h2> {eachDrink.userId?.name}</h2>
+                    <h3>{eachDrink.name}</h3>
+                    <p>{eachDrink.drink}</p>
+                    <p>{eachDrink.instructions}</p>
+                    <img src={eachDrink.image}></img>
+                    <h3> Likes: {eachDrink.likes}</h3>
+                    <button onClick={(e) => handleClick(eachDrink._id, i)} >Like 👍</button>
+                    <hr></hr>
+                </div>
+            )
+        })
+    }
+    console.log(drinks)
+    return (
+        <div className="container">
+            <div className="red-header"><p>All Drinks</p></div><br></br>
+            <br></br>
+            <ShowPosts />
+
+        </div>
+    );
+}
+
+export default AllDrinks;

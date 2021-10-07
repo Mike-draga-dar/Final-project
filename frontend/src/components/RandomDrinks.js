877d43b42ab1ad64ccf24da5c42808e77f7e6627
Dragan Cicic <your3mail>
2021-10-07 12:58:23 -0400

    liked random button

--- frontend/src/components/RandomDrinks.js
+++ frontend/src/components/RandomDrinks.js
@@ -1,21 +1,32 @@
 import actions from "../api";
 import { useState, useEffect } from "react"
 import axios from "axios";
-
+import heart from '../../src/heart.png'
 
 
 function RandomImages(props, i) {
-    const [images, setImages] = useState({});
+    // const [images, setImages] = useState([]);
+    const [drink, setDrink] = useState({})
 
 
     useEffect(async () => {
 
         let res = await actions.getAllDrinks({ skip: 0, limit: 450 })
-        setImages(res.data)
-
+        console.log(res.data)
+        //setImages(res.data)
+        setDrink(res.data[Math.floor(Math.random() * res.data.length)])
     }, [])
-    let random = Math.floor(Math.random() * images.length)
 
+
+
+
+    const handleClick = async (whichPostId, i) => {
+
+        let res = await actions.likePost(whichPostId)
+        console.log(res.data)
+        //setImages(res.data)
+        setDrink(res.data)
+    }
 
 
     return (
@@ -27,29 +38,38 @@ function RandomImages(props, i) {
             <br></br>
             <div className="drink">
                 <div className="drink-photo">
-                    <img src={images?.[random]?.image} className="single-drink-img"></img>
+                    <img src={drink?.image} className="single-drink-img"></img>
                 </div>
                 <div className="drink-info">
-                    <h1>{images?.[random]?.name}</h1>
+                    <h1>{drink?.name}</h1>
 
                     <h2>Ingredients</h2>
-                    {images?.[random]?.ingredients.map(eachIngredient => {
+                    {drink.ingredients && drink?.ingredients.map(eachIngredient => {
                         return (
                             <li>{eachIngredient}</li>
                         )
                     })}
 
                     <h2>Measurements</h2>
-                    {images?.[random]?.measurements.map(eachMeasurement => {
+                    {drink.measurements && drink?.measurements.map(eachMeasurement => {
                         return (
                             <li>{eachMeasurement}</li>
                         )
                     })}
 
                     <h2>Instructions</h2>
-                    {images?.[random]?.instructions}
+                    {drink?.instructions}
 
 
+                    <div className="like-button">
+                        <div className="like-left">
+                            <button disabled={drink?.likes && drink?.likes.includes(props.user._id)} onClick={(e) => handleClick(drink?._id, i)}><img src={heart}></img></button>
+                        </div>
+
+                        <div className="like-right">
+                            {drink?.likes && drink?.likes.length} Likes
+                        </div>
+                    </div>
 
                 </div>
             </div>
